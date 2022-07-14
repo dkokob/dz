a70e151451f50d298b3ef1a47f8c54dbcbd876c9
dkoko <dkokovanin@gmail.com>
2022-07-14 15:39:43 +0300

    dz3

---
+++ Dz3.cs
@@ -0,0 +1,12 @@
+Console.Write("Введите числo");
+int a = Convert.ToInt32(Console.ReadLine()); 
+
+int result = a % 2;
+if(result == 0)
+{
+  Console.Write("Yes");
+}
+else
+{
+  Console.Write("No");
+}
