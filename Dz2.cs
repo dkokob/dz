47ae157ed0c99afd91f2d4655793c797763121b4
dkoko <dkokovanin@gmail.com>
2022-07-14 15:38:41 +0300

    dz2

---
+++ Dz2.cs
@@ -0,0 +1,9 @@
+Console.Write("Введите 3 числа");
+int a = Convert.ToInt32(Console.ReadLine()); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if(max<b) max = b;
+if(max<c) max = c;
+
+Console.WriteLine(max);
