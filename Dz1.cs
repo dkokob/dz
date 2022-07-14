035f1a15cb4cdefd3e6b681804bd7466699c7c31
dkoko <dkokovanin@gmail.com>
2022-07-14 15:36:31 +0300

    start

---
+++ Dz1.cs
@@ -0,0 +1,13 @@
+Console.Write("Введите два числа");
+int a = Convert.ToInt32(Console.ReadLine()); 
+int b = Convert.ToInt32(Console.ReadLine()); 
+if(a>b)
+{
+    Console.Write( a);
+    Console.Write(" - большеe");
+}
+else
+{
+    Console.Write(b);
+    Console.Write(" - большеe");
+}
